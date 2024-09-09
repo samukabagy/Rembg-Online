d9787693e8e9c4ad5edf717235d3068e6ea085bb
samukabagy <samuel.fortes@bagy.com.br>
2024-09-09 18:13:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def main():
                    fixed_token=os.getenv('GOTIFY_APP_TOKEN'),
                    fixed_priority=9)
 
-    if st.sidebar.button('CLEARe'):
+    if st.sidebar.button('CLEARee'):
         st.session_state['key'] = K
         st.experimental_rerun()
     st.sidebar.markdown('---')
